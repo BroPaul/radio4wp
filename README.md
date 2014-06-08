85851e69b4cb9d97a48a1fcc9b6b276d5d3f85d2
Paul Allen <bropaul@qq.com>
2014-06-08 23:43:08 +0800

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+radio4wp
+========
+
+A radio widget for WordPress
+
+Just include the `index.php` file in the `footer.php` of your WordPress theme and enjoy.
+
+Modify `track.js` to play your own music.
+
+For more info, check out this post: http://www.bropaul.com/radio4wp
---
+++ index.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/radio4wp/style.css">
+<div id="bgmPlayer">
+	<div class="cover"></div>
+	<div id="playerWrapper" class="fold">
+		<div id="player">
+			<div class="ctrl">
+				<div class="tag">
+					<strong>歌曲</strong>
+					<span class="artist">歌手</span>
+					<span class="album">专辑</span>
+				</div>
+				<div class="control">
+					<div class="left">
+						<div class="rewind icon"></div>
+						<div class="playback icon playing"></div>
+						<div class="fastforward icon"></div>
+					</div>
+					<div class="volume right">
+						<div class="mute icon left"></div>
+						<div class="slider left">
+							<div class="pace"></div>
+						</div>
+					</div>
+				</div>
+				<div class="progress">
+					<div class="slider">
+						<div class="loaded"></div>
+						<div class="pace"></div>
+					</div>
+					<div class="timer left">0:00</div>
+					<div class="right">
+						<div class="repeat icon"></div>
+						<div class="shuffle icon"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="foldBtn" title="单击展开"><i></i></div>
+	</div>
+</div>
+<script src="http://libs.baidu.com/jqueryui/1.8.24/jquery-ui.min.js "></script>
+<script src="<?php echo get_template_directory_uri()?>/radio4wp/track.js"></script>
+<script src="<?php echo get_template_directory_uri()?>/radio4wp/script.js"></script>
+<div id="bg1"></div>
+<div id="bg2"></div>
